ade662d41709ff127bf46da62f1dd4019dae6393
George Gillams <george.gillams@skyscanner.net>
2020-01-02 12:07:02 +0000

    [NO-JIRA] Update license headers

--- watcher.js
+++ watcher.js
@@ -1,7 +1,7 @@
 /*
  * backpack-node-sass
  *
- * Copyright 2018 Skyscanner Ltd
+ * Copyright 2020 Skyscanner Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
